dfe7e97ad02c7f66455e478fa1c04e63b47b4ad3
MAZENHASS <149175284+MAZENHASS@users.noreply.github.com>
2025-08-13 21:06:25 +0300

    car_price.py

--- project/car_price_prediction/car_price.py
+++ project/car_price_prediction/car_price.py
@@ -1,57 +1,73 @@
 import pickle
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
+# Load model and data
+model = pickle.load(open('car_price.pkl', 'rb'))
+
+all = pd.read_csv('cleaned.csv')
+cg=all.copy()
+# Streamlit page
 st.set_page_config(page_title="Car Price Prediction", layout="wide")
 
 
-with open('car_price.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-all = pd.read_csv('cleaned.csv')
-st.image('car price.png')
-
-cg = all.copy()
-
 st.title('Car Price Prediction')
 st.sidebar.header('Feature Selection')
-st.sidebar.info('An easy app to predict car prices.')
+st.sidebar.info('An easy app to predict')
+st.image('car price.png')
 
-def back(name, original_vals, encoded_vals, container):
-    mapping = dict(zip(original_vals, encoded_vals))
-    selected = container.selectbox(name, original_vals)
+def back(name, k1, k2, container):
+    mapping = dict(zip(k1, k2))
+    selected = container.selectbox(name, k1)
     return mapping[selected]
 
-menu = []
-col1, col2 = st.columns(2)
+menu = {}
+col1, col2 ,col3= st.columns(3)
 d = 0
 
 for i in all.select_dtypes(include='object').columns:
     if d % 2 == 0:
-        menu.append(back(i, all[i].unique(), all[i + 'encoded'].unique(), col1))
+        menu[i + 'encoded'] = back(i, all[i].unique(), all[i + 'encoded'].unique(), col1)
     else:
-        menu.append(back(i, all[i].unique(), all[i + 'encoded'].unique(), col2))
+        menu[i + 'encoded'] = back(i, all[i].unique(), all[i + 'encoded'].unique(), col2)
     d += 1
 
+    all=all[all['Manufacturerencoded'] ==menu[ 'Manufacturerencoded']]
+all=cg.copy()
+
+all.drop('Price',inplace=True,axis=1)
+del cg
+
+
 encoded_cols = [col + 'encoded' for col in all.select_dtypes(include='object').columns]
+all[['Levy', 'Prod. year', 'Airbags']] = all[['Levy', 'Prod. year', 'Airbags']].astype(int)
+intnum=['Levy', 'Prod. year', 'Airbags']
 for i in all.select_dtypes(exclude='object').columns:
-    if i not in encoded_cols and i != 'Price':
-        if d % 2 == 0:
-            menu.append(col1.number_input(i, float(all[i].min()), float(all[i].max()), float(all[i].min())))
-        else:
-            menu.append(col2.number_input(i, float(all[i].min()), float(all[i].max()), float(all[i].min())))
-        d += 1
+       if i not in encoded_cols  :
+              if d % 2 == 0:
+                     if all[i].dtype=='float':
+                            menu[i] = col1.number_input(i, min_value=float(all[i].min()), max_value=float(all[i].max()),
+                                                        value=float(all[i].min()))
+                     else:
+                            menu[i] = col1.number_input(i, min_value=int(all[i].min()), max_value=int(all[i].max()),
+                                                        value=int(all[i].min()))
 
 
-cols = ['Manufacturer', 'Model', 'Category', 'Fuel type', 'Gear box type',
-        'Drive wheels', 'Color', 'Levy', 'Prod. year', 'Leather interior',
-        'Engine volume', 'Mileage', 'Cylinders', 'Airbags']
+              else:
 
-bb = dict(zip(cols, menu))
-wb = pd.DataFrame(bb, index=[0])
+                     if all[i].dtype == 'float':
+                            menu[i] = col2.number_input(i, min_value=float(all[i].min()), max_value=float(all[i].max()),
+                                                        value=float(all[i].min()))
+                     else:
+                            menu[i] = col2.number_input(i, min_value=int(all[i].min()), max_value=int(all[i].max()),
+                                                        value=int(all[i].min()))
+              d+=1
 
-if st.sidebar.button('Predict'):
-    price = model.predict(wb)[0]
-    st.sidebar.success(f" Predicted Price: {price:,.2f}")
+col3.markdown('(Dataset from kaggle )[https://www.kaggle.com/datasets/deepcontractor/car-price-prediction-challenge/data]')
+wb=pd.DataFrame(menu,index=[0])
 
-st.markdown("[Dataset from Kaggle](https://www.kaggle.com/datasets/deepcontractor/car-price-prediction-challenge/data)")
+click=st.sidebar.button('Predict ')
+if click:
+
+       wb_predict=model.predict(wb.values)
+       st.sidebar.success(wb_predict)
