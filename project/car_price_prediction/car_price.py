ae9109f028d7a16df280fa139e9f573ac3565e33
MAZENHASS <149175284+MAZENHASS@users.noreply.github.com>
2025-08-22 05:08:07 +0300

    car_price.py

--- project/car_price_prediction/car_price.py
+++ project/car_price_prediction/car_price.py
@@ -1,16 +1,11 @@
-import pickle
 import streamlit as st
-import pandas as pd
 
 
-all = pd.read_csv('cleaned.csv')
-cg=all.copy()
-# Streamlit page
-st.set_page_config(page_title="Car Price Prediction", layout="wide")
 
 
 st.title('Car Price Prediction')
 st.sidebar.header('Feature Selection')
 st.sidebar.info('An easy app to predict')
 st.image('car price.png')
+
 
